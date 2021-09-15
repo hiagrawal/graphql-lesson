51031b5ece47a8d811254c722b39506da42c72bf
hiagrawal <agrawal.hina13@gmail.com>
2021-09-15 19:56:48 +0530

    used compose(flowRight) HOC to call queries and mutations and passing it as props

--- src/components/cart-icon/cart-icon.container.jsx
+++ src/components/cart-icon/cart-icon.container.jsx
@@ -1,7 +1,9 @@
 
 import React from 'react';
-import { Mutation, Query } from 'react-apollo';
+//import { Mutation, Query } from 'react-apollo';
+import { graphql } from 'react-apollo';
 import {gql} from 'apollo-boost';
+import { flowRight } from 'lodash';
 
 import CartIcon from './cart-icon.component';
 
@@ -28,16 +30,36 @@ const GET_CART_ITEM_COUNT = gql`
 //     </Mutation>
 // )
 
-const CartIconContainer = () => (
-    <Query query={GET_CART_ITEM_COUNT}>
-    {
-        ({ data : {itemCount}}) => (<Mutation mutation={TOGGLE_CART_HIDDEN}>
-        {
-            toggleCartHidden => <CartIcon toggleCartHidden={toggleCartHidden} itemCount={itemCount} />
-        }
-        </Mutation>)
-    }
-    </Query>
+// const CartIconContainer = () => (
+//     <Query query={GET_CART_ITEM_COUNT}>
+//     {
+//         ({ data : {itemCount}}) => (<Mutation mutation={TOGGLE_CART_HIDDEN}>
+//         {
+//             toggleCartHidden => <CartIcon toggleCartHidden={toggleCartHidden} itemCount={itemCount} />
+//         }
+//         </Mutation>)
+//     }
+//     </Query>
+// );
+
+// export default CartIconContainer;
+
+//we can do the same thing using higher order compoenenst using compose and graphql
+//compose was earlier provided by react-apollo which has been removed now but we can achieve the same using floatRight from lodash
+//it is same as we use in redux
+//we call all our query and mutations using graphql 
+//for query, it returns as it used to return earlier in data object and then paramter
+//whereas for mutations, it returns as in mutate param
+//to change it, we have second argument where we can give various configurations
+//one such configuartion is name where we can give the specific name we want
+//and then we will get the reponse of these calls as props like we used to get in redux
+
+const CartIconContainer = ({ data : { itemCount }, toggleCartHidden }) => (
+    <CartIcon toggleCartHidden={toggleCartHidden} itemCount={itemCount} />
 );
 
-export default CartIconContainer;
+export default flowRight(
+    graphql(GET_CART_ITEM_COUNT),
+    graphql(TOGGLE_CART_HIDDEN, { name : 'toggleCartHidden' })
+)(CartIconContainer);
+
